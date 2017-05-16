2f4e06ca41918adebc7dafc811e11792c212a1cc
Craig Davis <cdavis@alertlogic.com>
2017-05-16 10:59:40 -0500

    Fix some minor spelling issues with regards to my process of lower casing method names. added some useful information for how to use the library

--- agilepoint/__init__.py
+++ agilepoint/__init__.py
@@ -6,7 +6,11 @@ from .workflow import Workflow
 # pylint: disable=too-few-public-methods
 
 class AgilePoint(object):
-    """AgilePoint API"""
+    """AgilePoint API
+
+    Host: https://fqdn-of-agilepoint-server:14490
+    Path: AgilePointServer
+    These are pretty self explanatory: username, password"""
     def __init__(self, host, path, username, password):
         url = '{}/{}'.format(host, path)
         self.agilepoint = Hammock(url, auth=(username, password))
